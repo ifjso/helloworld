fc993cb73b84ce50bf8ae522d6c4ca22ac5bea28
ifjso <ojsboy@hotmail.com>
2020-05-29 15:41:46 +0900

    global style 추가

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from 'react';
+import Layout from './src/components/layout';
+import { GlobalStyle } from './src/theme/global-style';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <>
+    <GlobalStyle />
+    <Layout>{element}</Layout>
+  </>
+);
