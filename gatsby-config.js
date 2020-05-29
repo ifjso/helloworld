26a13fb97a0f83d6ab140ae1c072202628a1f0f6
ifjso <ojsboy@hotmail.com>
2020-05-29 15:08:48 +0900

    styled components 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,8 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
     },
-    'gatsby-plugin-netlify-cms'
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-styled-components'
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
