223281873576e1890f5eafcf3e3b5d83a8a293ee
ifjso <ojsboy@hotmail.com>
2020-05-30 16:45:46 +0900

    SEO 삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Image from '../components/image';
-import SEO from '../components/seo';
 import 'normalize.css';
 
 const IndexPage = () => (
   <>
-    <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
