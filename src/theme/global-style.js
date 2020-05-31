c3b9eb8c807b9938076486233cae54fbb0303de3
ifjso <ojsboy@hotmail.com>
2020-05-31 13:43:27 +0900

    dark, light mode css 추가

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -1,4 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #1a1919;
+    color: #999
+  }
+
+  body {
+    transition: background-color 0.5s ease;
+  }
+
+  body.light-mode {
+    background-color: #fff;
+    color: #000;
+  }
+
+  body.dark-mode {
+    background-color: #1a1919;
+    color: #999;
+  }
 `;
