a3d843b232cc8ac440de31f4d547021888801b93
ifjso <ojsboy@hotmail.com>
2020-05-31 14:58:27 +0900

    chore: site meta data 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs'
+    title: `JaeSeok's Hello, World!`,
+    description: `Let me introduce myself.`,
+    author: 'ojsboy@hotmail.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
