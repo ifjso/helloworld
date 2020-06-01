3e8722f1d437830c40d3b56029ee318e8e1e47c6
ifjso <ojsboy@hotmail.com>
2020-06-01 18:11:58 +0900

    dark mode 버튼 초기화 문제점 수정 (#10)
    * fix: use-dark-mode 삭제 및 gatsby-plugin-dark-mode 추가
    
    * fix: dark mode 버튼 초기화 오류 수정
    
    * chore: version  up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 import DarkModeButton from '../DarkModeButton';
 import GithubButton from '../GithubButton';
 import {
@@ -10,19 +10,29 @@ import {
   Container
 } from './style';
 
-const Header = ({ siteTitle }) => (
-  <StyledHeader>
-    <Container>
-      <TitleWrapper>
-        <StyledLink to="/">{siteTitle}</StyledLink>
-      </TitleWrapper>
-      <ButtonWrapper>
-        <GithubButton size="1.8rem" />
-        <DarkModeButton size="2.2rem" />
-      </ButtonWrapper>
-    </Container>
-  </StyledHeader>
-);
+const Header = ({ siteTitle }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <StyledHeader>
+      <Container>
+        <TitleWrapper>
+          <StyledLink to="/">{siteTitle}</StyledLink>
+        </TitleWrapper>
+        {isClient ? (
+          <ButtonWrapper>
+            <GithubButton size="1.8rem" />
+            <DarkModeButton size="2.2rem" />
+          </ButtonWrapper>
+        ) : null}
+      </Container>
+    </StyledHeader>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string
