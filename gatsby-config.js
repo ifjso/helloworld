f9e805684fd3249879353a3e13dc685e9009afa1
dval20 <ojsboy@hotmail.com>
2020-06-06 23:40:17 +0900

    feat: gatsby  svg plugin 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/static/assets`
       }
     },
     {
@@ -43,6 +43,14 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/
+        }
+      }
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
@@ -51,7 +59,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'static/assets/gatsby-icon.png' // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-netlify-cms',
