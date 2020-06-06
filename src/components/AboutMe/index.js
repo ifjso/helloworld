b888d2b5f50f26d7a54746d26af36e119ccad441
dval20 <ojsboy@hotmail.com>
2020-06-06 15:39:24 +0900

    fix: about me 전체 스타일 수정

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { Container, Wrapper } from './style';
 
 const AboutMe = () => {
   const data = useStaticQuery(
@@ -20,74 +21,14 @@ const AboutMe = () => {
   const aboutMe = data.markdownRemark;
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#123123'
-      }}
-    >
-      <div
-        style={{
-          maxWidth: '960px',
-          width: '100%',
-          margin: '0 1rem',
-          padding: '2rem 0'
-        }}
-      >
-        <div style={{ textAlign: 'center', paddingBottom: '4rem' }}>
-          <h2
-            style={{
-              fontSize: '2.5em',
-              fontWeight: 300,
-              margin: 0
-            }}
-          >
-            About me
-          </h2>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ flex: '1 1', paddingRight: '40px' }}>
-            <h3
-              style={{
-                fontSize: '1.5em',
-                fontWeight: 400,
-                margin: '0',
-                paddingBottom: '0.4rem'
-              }}
-            >
-              {aboutMe.frontmatter.name}
-            </h3>
-            <h4
-              style={{
-                fontSize: '1.1em',
-                fontWeight: 400,
-                margin: '0',
-                paddingBottom: '1.0rem'
-              }}
-            >
-              {aboutMe.frontmatter.intro}
-            </h4>
-            <div
-              dangerouslySetInnerHTML={{ __html: aboutMe.html }}
-              style={{
-                fontSize: '0.9em',
-                lineHeight: 1.8,
-                listStyleType: 'none'
-              }}
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip"
-              style={{ width: '260px' }}
-            />
-          </div>
-        </div>
+    <Container>
+      <Wrapper>
+        <h1>{aboutMe.frontmatter.name}</h1>
+        <h2>{aboutMe.frontmatter.intro}</h2>
+        <div dangerouslySetInnerHTML={{ __html: aboutMe.html }} />
         {/* <h4>{aboutMe.frontmatter.tech_stack}</h4> */}
-      </div>
-    </div>
+      </Wrapper>
+    </Container>
   );
 };
 
---
+++ src/components/AboutMe/style.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 960px;
+  width: 100%;
+  box-sizing: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    margin: 0;
+    padding-bottom: 0.4rem;
+    font-size: 2.8em;
+    font-weight: 400;
+  }
+
+  h2 {
+    margin: 0;
+    padding-bottom: 2rem;
+    font-size: 1.8em;
+    font-weight: 400;
+  }
+
+  p {
+    margin: 0;
+    font-size: 1.3em;
+    line-height: 1.8;
+    list-style-type: none;
+  }
+`;
