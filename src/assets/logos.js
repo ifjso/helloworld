75334b0e0c6814183c4edc27e5185788d58f5bf2
dval20 <ojsboy@hotmail.com>
2020-06-07 22:29:59 +0900

    fix: tech logo 수정

---
+++ src/assets/logos.js
@@ -0,0 +1,33 @@
+import ExpressLogo from '../../static/assets/express-logo.svg';
+import GitLogo from '../../static/assets/git-logo.svg';
+import GraphqlLogo from '../../static/assets/graphql-logo.svg';
+import JavaLogo from '../../static/assets/java-logo.svg';
+import JavaScriptLogo from '../../static/assets/javascript-logo.svg';
+import JenkinsLogo from '../../static/assets/jenkins-logo.svg';
+import JunitLogo from '../../static/assets/junit-logo.svg';
+import MongodbLogo from '../../static/assets/mongodb-logo.svg';
+import MysqlLogo from '../../static/assets/mysql-logo.svg';
+import NodejsLogo from '../../static/assets/nodejs-logo.svg';
+import ReactLogo from '../../static/assets/react-logo.svg';
+import RedisLogo from '../../static/assets/redis-logo.svg';
+import SpringLogo from '../../static/assets/spring-logo.svg';
+import Html5Logo from '../../static/assets/html5-logo.svg';
+import Css3Logo from '../../static/assets/css3-logo.svg';
+
+export {
+  ExpressLogo,
+  GitLogo,
+  GraphqlLogo,
+  JavaLogo,
+  JavaScriptLogo,
+  JenkinsLogo,
+  JunitLogo,
+  MongodbLogo,
+  MysqlLogo,
+  NodejsLogo,
+  ReactLogo,
+  RedisLogo,
+  SpringLogo,
+  Html5Logo,
+  Css3Logo
+};
