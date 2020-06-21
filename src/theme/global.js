7ba64da025c4572081f3dafdc8a253a8b6f627cc
ifjso <ojsboy@hotmail.com>
2020-06-21 17:12:40 +0900

    fix: 깜빡임 문제로 darkmode transition 삭제

--- src/theme/global.js
+++ src/theme/global.js
@@ -9,15 +9,12 @@ export const Media = Object.freeze({ BASE: '1170px' });
 export const GlobalStyle = createGlobalStyle`
 
   html {
-    background-color: var(--black-russian);
-    color: var(--light-gray);
     ${FontFamily}
     ${FontWeight}
     ${Colors}
   }
 
   body {
-    transition: background-color 0.5s ease;
     background-color: var(--background-color);
     color: var(--color);
 
