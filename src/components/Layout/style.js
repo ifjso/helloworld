0a648643634f1ab68e0e334c3d8ab7da4ae1f65b
ifjso <ojsboy@hotmail.com>
2020-06-25 08:08:50 +0900

    fix: main, footer style 수정

---
+++ src/components/Layout/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import Devices from '../../utils/constants/devices';
+
+export const Main = styled.main`
+  padding-bottom: 2rem;
+
+  @media screen and (min-width: ${Devices.TABLET_WIDTH}px) {
+    padding-bottom: 3rem;
+  }
+`;
+
+export const Footer = styled.footer`
+  padding-bottom: 1rem;
+  font-size: 0.6em;
+  text-align: center;
+
+  @media screen and (min-width: ${Devices.TABLET_WIDTH}px) {
+    padding-bottom: 1.2rem;
+    font-size: 0.8em;
+  }
+`;
